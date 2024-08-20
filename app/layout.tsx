dc97f08ab7070f1e2d9e7610f38fbfc7d220532e
Bridger Tower <bridgertower@gmail.com>
2024-08-20 11:18:30 -0600

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import "./globals.css";
+
+import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 
-import { cn } from "@/lib/utils";
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+const font = FontSans({ subsets: ["latin"] });
 
 const metadata: Metadata = {
   title: "designengineer.fyi / Resources for Design Engineers",
@@ -18,24 +14,13 @@ const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
       </body>
